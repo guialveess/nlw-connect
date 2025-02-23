f95807d17282daf3ff7f630db2d87f367517ed71
guialveess <97guilherme.alves@gmail.com>
2025-02-23 16:28:02 -0300

    new features in front / conections with api

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,11 +1,19 @@
+import { InviteLinkInput } from '@/app/invite/[subscriberId]/invite-link-input'
+import { Ranking } from '@/app/invite/[subscriberId]/ranking'
+import { Stats } from '@/app/invite/[subscriberId]/stats'
 import Logo from '@/assets/logo.svg'
 import Image from 'next/image'
-import { InviteLinkInput } from './invite-link-input'
-import Ranking from './ranking'
-import Stats from './stats'
 
-export default function InvitePage() {
-  const inviteLink = 'http://localhost:3000/61627271128121712'
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string
+  }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -35,7 +43,7 @@ export default function InvitePage() {
 
           <InviteLinkInput inviteLink={inviteLink} />
 
-          <Stats />
+          <Stats subscriberId={subscriberId} />
         </div>
       </div>
       <Ranking />
